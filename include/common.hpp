1840844665adee77e8701535d6f6a2f823a051c1
Aditya <aydeshpande20@gmail.com>
2020-03-23 16:56:34 +0000

    Coronavirus has screwed with my memory

---
+++ include/common.hpp
@@ -0,0 +1,18 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+//variables available to all files:
+#include "ast.hpp"
+
+
+#include <vector>
+#include <string>
+
+static std::vector<NodePtr> global_variables_values;
+static std::vector<std::string> global_variables_names;
+
+static int currentIndent = 0;
+static int function_call_num;
+static int main_returned;
+
+#endif
