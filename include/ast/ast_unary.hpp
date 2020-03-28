1cedeb6cea8edc448e22d4c7a407114c2c3f04f9
Aditya <aydeshpande20@gmail.com>
2020-03-28 21:51:51 +0000

    Added unary compilation functions

--- include/ast/ast_unary.hpp
+++ include/ast/ast_unary.hpp
@@ -7,10 +7,10 @@
 class Unary
     : public AST_Node
 {
-private:
+protected:
     std::string var;
     std::string position;
-protected:
+
     Unary( std::string _var, std::string _position)
         : var(_var), position(_position)
     {}
@@ -51,6 +51,7 @@ public:
             dst << var<<getOpcode();
         }
     }
+
 };
 
 
@@ -66,6 +67,87 @@ public:
     virtual const char *getOpcode() const override
     { return "++"; }
 
+    virtual void toMips(std::ostream &dst, System &mySystem, int destReg) const override
+    {
+        if(position == "pre")
+        {
+            std::vector<int> scratchRegs = mySystem.temp_freeRegLookup();
+            int stackSize = (stackVarCount*4) + paramCount + 62;
+            mySystem.lockReg(scratchRegs[0]);
+            
+
+            int varAddress = mySystem.systemMemory.variableSearch(var, currentIndent);
+
+            if(varAddress > 0) //local variable
+            {
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " <<varAddress <<"($fp)"<<"\t\t #loading local variable " << var <<std::endl;
+                dst << "\t" << "nop" << std::endl;
+                dst << "\t" << "addiu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", 1" << "\t\t #incrementing by 1 due to ++ operator" << std::endl;
+                dst << "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) <<", "<< varAddress <<"($fp)"<<"\t\t #storing " << var << " after increment"<<std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " <<varAddress <<"($fp)" <<std::endl;
+                dst << "\t" << "nop" << std::endl;
+            }
+            else if(mySystem.lookupParameter(var) > 1)
+            {
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << ((mySystem.lookupParameter(var))*4 + stackSize) << "($fp)" << "\t\t #loading function parameter " << var << std::endl;
+                dst << "\t" << "nop" << std::endl;
+                dst << "\t" << "addiu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", 1" << "\t\t #incrementing by 1 due to ++ operator" << std::endl;
+                dst << "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) <<", "<< ((mySystem.lookupParameter(var))*4 + stackSize) <<"($fp)"<<"\t\t #storing " << var << " after increment"<<std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << ((mySystem.lookupParameter(var))*4 + stackSize) << "($fp)" << std::endl;
+                dst << "\t" << "nop" << std::endl;
+            
+            }
+            else
+            {
+                dst << "\t" << "lui" << "\t" << "$" << mySystem.getRegName(destReg) << ", %hi(" << var << ")" << "\t\t #loading global variable: " << var << std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", %lo(" << var << ")($" << mySystem.getRegName(destReg) << ")" << std::endl;
+                dst << "\t" << "nop" << std::endl;
+                dst << "\t" << "addu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", 1" <<"\t\t #incrementing by 1 due to ++ operator" << std::endl;
+                dst<< "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << " %lo("<< var << ")($" << mySystem.getRegName(destReg) <<")" << "\t\t #storing " << var << " after increment" << std::endl;
+                dst << "\t" << "lui" << "\t" << "$" << mySystem.getRegName(destReg) << ", %hi(" << var << ")" <<  std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", %lo(" << var << ")($" << mySystem.getRegName(destReg) << ")" << std::endl;
+                dst << "\t" << "nop" << std::endl;
+            }
+        
+            mySystem.unlockReg(scratchRegs[0]);
+        }
+        else
+        {
+            std::vector<int> scratchRegs = mySystem.temp_freeRegLookup();
+            int stackSize = (stackVarCount*4) + paramCount + 62;
+            mySystem.lockReg(scratchRegs[0]);
+            
+
+            int varAddress = mySystem.systemMemory.variableSearch(var, currentIndent);
+
+            if(varAddress > 0) //local variable
+            {
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " <<varAddress <<"($fp)"<<"\t\t #loading local variable " << var <<std::endl;
+                dst << "\t" << "nop" << std::endl;
+                dst << "\t" << "addiu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", 1" << "\t\t #incrementing by 1 due to ++ operator" << std::endl;
+                dst << "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) <<", "<< varAddress <<"($fp)"<<"\t\t #storing " << var << " after increment"<<std::endl;
+            }
+            else if(mySystem.lookupParameter(var) > 1)
+            {
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << ((mySystem.lookupParameter(var))*4 + stackSize) << "($fp)" << "\t\t #loading function parameter " << var << std::endl;
+                dst << "\t" << "nop" << std::endl;
+               dst << "\t" << "addiu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", 1" << "\t\t #incrementing by 1 due to ++ operator" << std::endl;
+                dst << "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) <<", "<< ((mySystem.lookupParameter(var))*4 + stackSize) <<"($fp)"<<"\t\t #storing " << var << " after increment"<<std::endl;
+            
+            }
+            else
+            {
+                dst << "\t" << "lui" << "\t" << "$" << mySystem.getRegName(destReg) << ", %hi(" << var << ")" << "\t\t #loading global variable: " << var << std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", %lo(" << var << ")($" << mySystem.getRegName(destReg) << ")" << std::endl;
+                dst << "\t" << "addu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", 1" <<"\t\t #incrementing by 1 due to ++ operator" << std::endl;
+                dst<< "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << " %lo("<< var << ")($" << mySystem.getRegName(destReg) <<")" << "\t\t #storing " << var << " after increment" << std::endl;
+            }
+        
+            mySystem.unlockReg(scratchRegs[0]);
+     
+        }
+    }
+
 };
 
 class DecrementOperator
@@ -78,6 +160,87 @@ public:
 
     virtual const char *getOpcode() const override
     { return "--"; }
+
+    virtual void toMips(std::ostream &dst, System &mySystem, int destReg) const override
+    {
+        if(position == "pre")
+        {
+            std::vector<int> scratchRegs = mySystem.temp_freeRegLookup();
+            int stackSize = (stackVarCount*4) + paramCount + 62;
+            mySystem.lockReg(scratchRegs[0]);
+            
+
+            int varAddress = mySystem.systemMemory.variableSearch(var, currentIndent);
+
+            if(varAddress > 0) //local variable
+            {
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " <<varAddress <<"($fp)"<<"\t\t #loading local variable " << var <<std::endl;
+                dst << "\t" << "nop" << std::endl;
+                dst << "\t" << "addiu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", -1" << "\t\t #decrementing by 1 due to -- operator" << std::endl;
+                dst << "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) <<", "<< varAddress <<"($fp)"<<"\t\t #storing " << var << " after decrement"<<std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " <<varAddress <<"($fp)" <<std::endl;
+                dst << "\t" << "nop" << std::endl;
+            }
+            else if(mySystem.lookupParameter(var) > 1)
+            {
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << ((mySystem.lookupParameter(var))*4 + stackSize) << "($fp)" << "\t\t #loading function parameter " << var << std::endl;
+                dst << "\t" << "nop" << std::endl;
+                dst << "\t" << "addiu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", -1" << "\t\t #decrementing by 1 due to -- operator" << std::endl;
+                dst << "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) <<", "<< ((mySystem.lookupParameter(var))*4 + stackSize) <<"($fp)"<<"\t\t #storing " << var << " after decrement"<<std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << ((mySystem.lookupParameter(var))*4 + stackSize) << "($fp)" << std::endl;
+                dst << "\t" << "nop" << std::endl;
+            
+            }
+            else
+            {
+                dst << "\t" << "lui" << "\t" << "$" << mySystem.getRegName(destReg) << ", %hi(" << var << ")" << "\t\t #loading global variable: " << var << std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", %lo(" << var << ")($" << mySystem.getRegName(destReg) << ")" << std::endl;
+                dst << "\t" << "nop" << std::endl;
+                dst << "\t" << "addu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", -1" <<"\t\t #decrementing by -1 due to -- operator" << std::endl;
+                dst<< "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << " %lo("<< var << ")($" << mySystem.getRegName(destReg) <<")" << "\t\t #storing " << var << " after decrement" << std::endl;
+                dst << "\t" << "lui" << "\t" << "$" << mySystem.getRegName(destReg) << ", %hi(" << var << ")" <<  std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", %lo(" << var << ")($" << mySystem.getRegName(destReg) << ")" << std::endl;
+                dst << "\t" << "nop" << std::endl;
+            }
+        
+            mySystem.unlockReg(scratchRegs[0]);
+        }
+        else
+        {
+            std::vector<int> scratchRegs = mySystem.temp_freeRegLookup();
+            int stackSize = (stackVarCount*4) + paramCount + 62;
+            mySystem.lockReg(scratchRegs[0]);
+            
+
+            int varAddress = mySystem.systemMemory.variableSearch(var, currentIndent);
+
+            if(varAddress > 0) //local variable
+            {
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " <<varAddress <<"($fp)"<<"\t\t #loading local variable " << var <<std::endl;
+                dst << "\t" << "nop" << std::endl;
+                dst << "\t" << "addiu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", -1" << "\t\t #decrementing by 1 due to -- operator" << std::endl;
+                dst << "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) <<", "<< varAddress <<"($fp)"<<"\t\t #storing " << var << " after increment"<<std::endl;
+            }
+            else if(mySystem.lookupParameter(var) > 1)
+            {
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << ((mySystem.lookupParameter(var))*4 + stackSize) << "($fp)" << "\t\t #loading function parameter " << var << std::endl;
+                dst << "\t" << "nop" << std::endl;
+               dst << "\t" << "addiu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", -1" << "\t\t #decrementing by 1 due to -- operator" << std::endl;
+                dst << "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) <<", "<< ((mySystem.lookupParameter(var))*4 + stackSize) <<"($fp)"<<"\t\t #storing " << var << " after decrement"<<std::endl;
+            
+            }
+            else
+            {
+                dst << "\t" << "lui" << "\t" << "$" << mySystem.getRegName(destReg) << ", %hi(" << var << ")" << "\t\t #loading global variable: " << var << std::endl;
+                dst << "\t" << "lw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", %lo(" << var << ")($" << mySystem.getRegName(destReg) << ")" << std::endl;
+                dst << "\t" << "addu" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", $" << mySystem.getRegName(scratchRegs[0]) << ", -1" <<"\t\t #incrementing by -1 due to -- operator" << std::endl;
+                dst<< "\t" << "sw" << "\t" << "$" << mySystem.getRegName(scratchRegs[0]) << ", " << " %lo("<< var << ")($" << mySystem.getRegName(destReg) <<")" << "\t\t #storing " << var << " after decrement" << std::endl;
+            }
+        
+            mySystem.unlockReg(scratchRegs[0]);
+     
+        }
+    }
 
 };
 
@@ -107,6 +270,11 @@ public:
         val->toPython(dst);
     }
 
+    virtual void toMips(std::ostream &dst, System &mySystem, int destReg) const override
+    {
+        val->toMips(dst, mySystem, destReg);
+        dst << "\t" << "sltiu" << "\t" << "$" << mySystem.getRegName(destReg) << ", 1" << "\t\t # logical not operator" << std::endl;
+    }
  
 
 };
@@ -137,6 +305,20 @@ public:
         
         dst << "-";
         val->toPython(dst);
+    }
+
+    virtual void toMips(std::ostream &dst, System &mySystem, int destReg) const override
+    {
+        if(isGlobal)
+        {
+            int v = val->evaluate(mySystem);
+            dst << -v;
+        }
+        else
+        {
+            val->toMips(dst, mySystem, destReg);
+            dst << "\t" << "sub" << "\t" << "$" << mySystem.getRegName(destReg) << ", $zero, $" << mySystem.getRegName(destReg) << "\t\t #negative operator " << std::endl;
+        }
     }
 
  
